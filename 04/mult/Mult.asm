0761fe318e36d299d22285aec944da36c3bf2c2d
haryoiro <mizusecocolte@gmail.com>
2021-02-08 10:52:23 +0900

    04 Mult.asm solution

--- 04/mult/Mult.asm
+++ 04/mult/Mult.asm
@@ -9,4 +9,25 @@
 // This program only needs to handle arguments that satisfy
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
-// Put your code here.
+  @i
+  M=0
+  @R2
+  M=0
+(LOOP)
+  @i
+  D=M     // D=i
+  @R0
+  D=M-D   // D=R0-i
+  @END
+  D;JLE   // IF (R0-i) <= 0 then goto END
+  @R1
+  D=M     // D=R1
+  @R2
+  M=M+D   // sum=sum+R1
+  @i
+  M=M+1   // i=i+1
+  @LOOP
+  0;JMP   // GOTO LOOP
+(END)
+  @END
+  0;JMP   // infinite loop
