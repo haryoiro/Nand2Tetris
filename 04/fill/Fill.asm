a1c447ead62ea7ea801afb1e26663058ca553391
haryoiro <mizusecocolte@gmail.com>
2021-02-17 17:08:52 +0900

    04 Fill.asm solution

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -6,9 +6,46 @@
 // Runs an infinite loop that listens to the keyboard input.
 // When a key is pressed (any key), the program blackens the screen,
 // i.e. writes "black" in every pixel;
-// the screen should remain fully black as long as the key is pressed. 
+// the screen should remain fully black as long as the key is pressed.
 // When no key is pressed, the program clears the screen, i.e. writes
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+(INIT)
+  @8192
+  D=A
+  @i
+  M=D
+
+(LOOP)
+  @i
+  M=M-1
+  D=M       // RAM[i-1]
+
+  @INIT
+  D;JLT     // if i < 0 goto INIT
+
+  @KBD
+  D=M
+  @WHITE
+  D;JEQ
+  @BLACK
+  D;JMP
+
+(WHITE)
+  @SCREEN
+  D=A
+  @i
+  A=D+M
+  M=0
+  @LOOP
+  0;JMP
+
+(BLACK)
+  @SCREEN
+  D=A
+  @i
+  A=D+M
+  M=-1
+  @LOOP
+  0;JMP
